f1355d9710a334687844e326bcf034691621d28f
Martin Ficzel <ficzel@sitegeist.de>
2015-11-09 13:35:17 +0100

    [TASK] added clone command-controller
    - ./flow clone:list - show the list of presets in Settings.yaml:Sitegeis.MagicWand.clonePresets
    - ./flow clone:preset - clone from a given preset in Settings.yaml:Sitegeis.MagicWand.clonePresets
    - ./flow clone:remoteHost clone from given host, user, path, port and context
    
    The settings of the local and remote host are determined via the configuration manager.

--- Classes/Sitegeist/MagicWand/Command/CloneCommandController.php
+++ Classes/Sitegeist/MagicWand/Command/CloneCommandController.php
@@ -6,14 +6,22 @@ namespace Sitegeist\MagicWand\Command;
  *                                                                        *
  *                                                                        */
 
+use Flowpack\Behat\Tests\Behat\FlowContext;
 use TYPO3\Flow\Annotations as Flow;
 use TYPO3\Flow\Configuration\ConfigurationManager;
 use TYPO3\Flow\Utility\Arrays;
+use TYPO3\Flow\Core\Bootstrap;
 
 /**
  * @Flow\Scope("singleton")
  */
 class CloneCommandController extends \TYPO3\Flow\Cli\CommandController {
+
+	/**
+	 * @Flow\Inject
+	 * @var Bootstrap
+	 */
+	protected $bootstrap;
 
 	/**
 	 * @Flow\Inject
@@ -26,6 +34,20 @@ class CloneCommandController extends \TYPO3\Flow\Cli\CommandController {
 	 * @Flow\InjectConfiguration("clonePresets")
 	 */
 	protected $clonePresets;
+
+	/**
+	 * Show the list of predefined clone configurations
+	 */
+	public function listCommand() {
+		if ($this->clonePresets) {
+			foreach ($this->clonePresets as $presetName => $presetConfiguration) {
+				$this->outputHeadLine($presetName);
+				foreach ($presetConfiguration as $key => $value){
+					$this->outputLine(' - ' . $key . ': ' . $value);
+				}
+			}
+		}
+	}
 
 	/**
 	 * Clone a flow setup as specified in Settings.yaml (Sitegeist.MagicWand.clonePresets ...)
@@ -58,12 +80,22 @@ class CloneCommandController extends \TYPO3\Flow\Cli\CommandController {
 	 * @param string $context
 	 */
 	public function remoteHostCommand($host, $user, $path, $port=22, $context='Production') {
+		// read local configuration
+		$localPersistenceConfiguration = $this->configurationManager->getConfiguration('Settings', 'TYPO3.Flow.persistence.backendOptions');
+		$localDataPersistentPath = FLOW_PATH_ROOT . 'Data/Persistent';
 
-		$this->outputLine(' - Host: ' . $host);
-		$this->outputLine(' - User: ' . $user);
-		$this->outputLine(' - Port: ' . $port);
-		$this->outputLine(' - Path: ' . $path);
-		$this->outputLine(' - Context: ' . $context);
+		// read remote configuration
+		$fetchRemotePersistenceConfigurationViaSSH = 'ssh -p ' . $port . ' ' . $user . '@'. $host . ' "cd ' . $path . '; FLOW_CONTEXT=' . $context . ' ./flow configuration:show --type Settings --path TYPO3.Flow.persistence.backendOptions;"';
+		$this->outputHeadLine($fetchRemotePersistenceConfigurationViaSSH);
+		$remotePersistenceConfigurationYaml =  shell_exec($fetchRemotePersistenceConfigurationViaSSH);
+		if ($remotePersistenceConfigurationYaml){
+			$remotePersistenceConfiguration = \Symfony\Component\Yaml\Yaml::parse($remotePersistenceConfigurationYaml);
+		}
+		$remoteDataPersistentPath = $path . '/Data/Persistent';
+
+		#################
+		# Are you sure? #
+		#################
 
 		$this->outputLine( "Are you sure you want to do this?  Type 'yes' to continue: ");
 		$handle = fopen ("php://stdin","r");
@@ -76,34 +108,11 @@ class CloneCommandController extends \TYPO3\Flow\Cli\CommandController {
 			$this->outputLine();
 		}
 
-		$localSettings = $this->configurationManager->getConfiguration('Settings');
-
-		// local configuration
-		$localPersistenceConfiguration = Arrays::getValueByPath($localSettings, 'TYPO3.Flow.persistence.backendOptions');
-		$localDataPersistentPath = FLOW_PATH_ROOT . '/Data/Persistent';
-		$this->outputHeadLine('LocalSettings:');
-		$this->outputLine(' - PersistentDataPath: ' . $localDataPersistentPath);
-		foreach ($localPersistenceConfiguration as $key => $value){
-			$this->outputLine(' - ' . $key . ': ' . $value);
-		}
-
-		// remote configuration
-		$fetchRemotePersistenceConfigurationViaSSH = 'ssh -p ' . $port . ' ' . $user . '@'. $host . ' "cd ' . $path . '; FLOW_CONTEXT=' . $context . ' ./flow configuration:show --type Settings --path TYPO3.Flow.persistence.backendOptions;"';
-		$this->outputHeadLine($fetchRemotePersistenceConfigurationViaSSH);
-		$remotePersistenceConfigurationYaml =  shell_exec($fetchRemotePersistenceConfigurationViaSSH);
-		if ($remotePersistenceConfigurationYaml){
-			$remotePersistenceConfiguration = \Symfony\Component\Yaml\Yaml::parse($remotePersistenceConfigurationYaml);
-		}
-
-		$remoteDataPersistentPath = $path . '/Data/Persistent';
-		$this->outputHeadLine('RemoteSettings:');
-		$this->outputLine(' - PersistentDataPath: ' . $remoteDataPersistentPath);
-		foreach ($remotePersistenceConfiguration as $key => $value){
-			$this->outputLine(' - ' . $key . ': ' . $value);
-		}
+		#######################
+		# Check Configuration #
+		#######################
 
 		$this->outputHeadLine('1. Check Configuration');
-
 		if ($remotePersistenceConfiguration['driver'] != 'pdo_mysql' && $localPersistenceConfiguration['driver'] != 'pdo_mysql' ) {
 			$this->outputLine(' only mysql is supported');
 			$this->quit(1);
@@ -112,39 +121,57 @@ class CloneCommandController extends \TYPO3\Flow\Cli\CommandController {
 			$this->outputLine(' the databases have to use the same charset');
 			$this->quit(1);
 		}
-
 		$this->outputLine(' - Configuration seems ok ...');
 
-		$this->outputHeadLine('2. Clear local database');
+		######################
+		#  Transfer Database #
+		######################
 
-		$mysqli = new \mysqli($localPersistenceConfiguration['host'], $localPersistenceConfiguration['user'], $localPersistenceConfiguration['password'], $localPersistenceConfiguration['dbname']);
-		if ($mysqli->connect_errno) {
-			$this->outputLine( "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error);
-			$this->quit(1);
-		}
-		$tableNameQuery = $mysqli->query("SELECT table_name AS name FROM information_schema.tables WHERE table_schema = DATABASE()");
-		for ($rowNumber = $tableNameQuery->num_rows - 1; $rowNumber >= 0; $rowNumber--) {
-			$tableNameQuery->data_seek($rowNumber);
-			$tableNameRow = $tableNameQuery->fetch_assoc();
-			$tableName = $tableNameRow['name'];
-			$mysqli->query('SET foreign_key_checks = 0');
-			$mysqli->query('DROP TABLE '. $tableName);
-			$this->outputLine( ' -  delete table ' . $tableName);
-		}
-
-		$this->outputHeadLine('3. Transfer database Contents');
-		$transferDatabaseViaSSH = 'ssh -p ' . $port . ' ' . $user . '@'. $host . ' "mysqldump --host=' . $remotePersistenceConfiguration['host'] . ' --user=' . $remotePersistenceConfiguration['user'] . ' --password=' . $remotePersistenceConfiguration['password'] . ' ' . $remotePersistenceConfiguration['dbname'] . '" | mysql --host=' . $localPersistenceConfiguration['host'] . ' --user=' . $localPersistenceConfiguration['user'] . ' --password=' . $localPersistenceConfiguration['password'] . ' ' . $localPersistenceConfiguration['dbname'];
-		$this->outputLine($transferDatabaseViaSSH);
-		$databaseTransferResult = shell_exec($transferDatabaseViaSSH);
+		$this->outputHeadLine('2. Transfer Database');
+		$transferDatabaseCommand = 'ssh -p ' . $port . ' ' . $user . '@'. $host . ' "mysqldump --add-drop-table --host=' . $remotePersistenceConfiguration['host'] . ' --user=' . $remotePersistenceConfiguration['user'] . ' --password=' . $remotePersistenceConfiguration['password'] . ' ' . $remotePersistenceConfiguration['dbname'] . '" | mysql --host=' . $localPersistenceConfiguration['host'] . ' --user=' . $localPersistenceConfiguration['user'] . ' --password=' . $localPersistenceConfiguration['password'] . ' ' . $localPersistenceConfiguration['dbname'];
+		$this->outputLine($transferDatabaseCommand);
+		$databaseTransferResult = shell_exec($transferDatabaseCommand);
 		$this->outputLine($databaseTransferResult);
 
-		$this->outputHeadLine('4. Rsync Data/Persistent folders');
-		// $transferDatabaseViaSSH = 'ssh -p ' . $port . ' ' . $user . '@'. $host . ' "mysqldump --host=' . $remotePersistenceConfiguration['host'] . ' --user=' . $remotePersistenceConfiguration['user'] . ' --password=' . $remotePersistenceConfiguration['password'] . ' ' . $remotePersistenceConfiguration['dbname'] . '" | mysql --host=' . $localPersistenceConfiguration['host'] . ' --user=' . $localPersistenceConfiguration['user'] . ' --password=' . $localPersistenceConfiguration['password'] . ' ' . $localPersistenceConfiguration['dbname'];
-		// $databaseTransferResult = shell_exec($transferDatabaseViaSSH);
+		##################
+		# Transfer Files #
+		##################
 
+		$this->outputHeadLine('3. Transfer Files');
+		$transferFilesCommand = 'rsync -e "ssh -p ' . $port . '" -kLr ' . $user . '@'. $host . ':' . $remoteDataPersistentPath . '/* ' . $localDataPersistentPath;
+		$this->outputLine($transferFilesCommand);
+		$transferFilesResult = shell_exec($transferFilesCommand);
+		$this->outputLine($transferFilesResult);
 
-		$this->outputHeadLine('5. clear caches');
+		################
+		# Clear Caches #
+		################
 
+		$this->outputHeadLine('4. Clear Caches');
+		$flushCachesCommand = 'FLOW_CONTEXT=' . $this->bootstrap->getContext() . ' ./flow flow:cache:flush';
+		$this->outputLine($flushCachesCommand);
+		$flushCachesResult = shell_exec($flushCachesCommand);
+		$this->outputLine($flushCachesResult);
+
+		##############
+		# Migrate DB #
+		##############
+
+		$this->outputHeadLine('5. Migrate cloned DB');
+		$migrateDbCommand = 'FLOW_CONTEXT=' . $this->bootstrap->getContext() . ' ./flow doctrine:migrate';
+		$this->outputLine($migrateDbCommand);
+		$migrateDbResult = shell_exec($migrateDbCommand);
+		$this->outputLine($migrateDbResult);
+
+		#####################
+		# Publish Resources #
+		#####################
+
+		$this->outputHeadLine('6. Publish Resources');
+		$publishResourcesCommand = 'FLOW_CONTEXT=' . $this->bootstrap->getContext() . ' ./flow resource:publish';
+		$this->outputLine($publishResourcesCommand);
+		$publishResourcesResult = shell_exec($publishResourcesCommand);
+		$this->outputLine($publishResourcesResult);
 	}
 
 	protected function outputHeadLine($line) {
