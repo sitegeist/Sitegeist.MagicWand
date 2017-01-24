68fd7088bf9bbd26a4144739b412677f3ac772d3
Max Strübing <struebing@sitegeist.de>
2017-01-24 11:21:49 +0100

    TASK: Add progress parameter to rsync
    so that you can see that something is happening

--- Classes/Sitegeist/MagicWand/Command/CloneCommandController.php
+++ Classes/Sitegeist/MagicWand/Command/CloneCommandController.php
@@ -228,7 +228,7 @@ class CloneCommandController extends AbstractCommandController
 
         $this->outputHeadLine('Transfer Files');
         $this->executeLocalShellCommand(
-            'rsync -e "ssh -p %s" -kLrz %s@%s:%s/* %s',
+            'rsync -e "ssh -p %s" -kLrz --progress %s@%s:%s/* %s',
             [
                 $port,
                 $user,
